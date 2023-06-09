c2f4e9cb5d82637ed3326dd9da32a4b160abe2e9
vominhcuong2501 <minhcuongvo2501@gmail.com>
2023-06-09 18:39:13 +0700

    +Add: text aplly mobile

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -4,20 +4,30 @@ import Menu from "../components/menu/menu";
 import Sidebar from "../components/sidebar/sidebar";
 import TaskDetailModal from "../components/modal/modal";
 import Header from "../components/header/header";
+import "./text-mobile.css"
 
 export default function HomeLayout() {
+  const windowWidth = window.innerWidth;
   return (
-    <div className="jira container-fluid p-0">
-      <Sidebar />
-      <Menu />
-      <div className="container p-0 m-0">
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <Header />
-          <Outlet />
-        </div>
-      </div>
+    <>
+      {windowWidth > 992 ? (
+        <div className="jira container-fluid p-0">
+          <Sidebar />
+          <Menu />
+          <div className="container p-0 m-0">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <Header />
+              <Outlet />
+            </div>
+          </div>
 
-      <TaskDetailModal />
-    </div>
+          <TaskDetailModal />
+        </div>
+      ) : (
+        <h2 data-text="&nbsp;♣ Mobile is not display ♠&nbsp;">
+          &nbsp;♣ Mobile is not display ♠&nbsp;
+        </h2>
+      )}
+    </>
   );
 }
