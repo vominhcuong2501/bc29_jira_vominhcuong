035e8de41c16b6a7b32b56d8bcd59510b2d3e143
VÕ MINH CƯỜNG <102784644+vominhcuong2501@users.noreply.github.com>
2023-06-12 11:03:53 +0700

    +Fix: breadcumb project detail

--- src/components/main/main-header.jsx
+++ src/components/main/main-header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function MainHeader(props) {
+  const styleCursor = {
+    cursor: "pointer",
+  };
+  const navigate = useNavigate();
   return (
     <div className="header">
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb" style={{ backgroundColor: "white" }}>
-          <li className="breadcrumb-item">Project</li>
-          <li className="breadcrumb-item">Project management</li>
+          <li
+            className="breadcrumb-item"
+            style={styleCursor}
+            onClick={() => navigate("/")}
+          >
+            Project management
+          </li>
           <li className="breadcrumb-item active" aria-current="page">
             {props.projectName}
           </li>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,62 +1,67 @@
 import { lazy } from "react";
 import { useRoutes } from "react-router-dom";
-const HomeLayout = lazy(() => import("../layouts/home"))
-const Login = lazy(() => import("../pages/login/login"))
-const Register = lazy(() => import("../pages/register/register"))
-const ProjectDetail = lazy(() => import("../pages/project-detail/project-detail"))
-const CreateProject = lazy(() => import("../pages/create-project/create-project"))
-const AuthGuard = lazy(() => import("../pages/guards/auth.guard"))
-const NoAuthGuard = lazy(() => import("../pages/guards/noAuth.guard"))
-const ProjectManagement = lazy(() => import("../pages/project-management/project-management"))
-const UserManagement = lazy(() => import("../pages/user-management/user-management"))
+const HomeLayout = lazy(() => import("../layouts/home"));
+const Login = lazy(() => import("../pages/login/login"));
+const Register = lazy(() => import("../pages/register/register"));
+const ProjectDetail = lazy(() =>
+  import("../pages/project-detail/project-detail")
+);
+const CreateProject = lazy(() =>
+  import("../pages/create-project/create-project")
+);
+const AuthGuard = lazy(() => import("../pages/guards/auth.guard"));
+const NoAuthGuard = lazy(() => import("../pages/guards/noAuth.guard"));
+const ProjectManagement = lazy(() =>
+  import("../pages/project-management/project-management")
+);
+const UserManagement = lazy(() =>
+  import("../pages/user-management/user-management")
+);
 
 export default function Router() {
-    const routing = useRoutes([
+  const routing = useRoutes([
+    {
+      path: "/",
+      element: <AuthGuard />,
+      children: [
         {
-            path: "/",
-            element: <AuthGuard />,
-            children: [
-                {
-                    path: "/",
-                    element: <HomeLayout />,
-                    children: [
-                        {
-                            path: "/",
-                            element: <ProjectManagement />,
-                        },
-                        {
-                            path: "/create-project",
-                            element: <CreateProject />,
-                        },
-                        {
-                            path: "/project-detail/:projectId",
-                            element: <ProjectDetail />,
-                        },
-                        {
-                            path: "/user-management",
-                            element: <UserManagement />,
-                        },
-                    ]
-                },
-
-            ]
+          path: "/",
+          element: <HomeLayout />,
+          children: [
+            {
+              path: "/",
+              element: <ProjectManagement />,
+            },
+            {
+              path: "/create-project",
+              element: <CreateProject />,
+            },
+            {
+              path: "/project-detail/:projectId",
+              element: <ProjectDetail />,
+            },
+            {
+              path: "/user-management",
+              element: <UserManagement />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: <NoAuthGuard />,
+      children: [
+        {
+          path: "/login",
+          element: <Login />,
         },
         {
-            path: "/",
-            element: <NoAuthGuard />,
-            children: [
-                {
-                    path: "/login",
-                    element: <Login />,
-                },
-                {
-                    path: "/register",
-                    element: <Register />,
-                },
-
-            ]
+          path: "/register",
+          element: <Register />,
         },
-
-    ]);
-    return routing;
+      ],
+    },
+  ]);
+  return routing;
 }
